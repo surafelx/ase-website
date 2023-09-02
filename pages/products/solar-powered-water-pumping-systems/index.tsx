43369cac1e41259e6e8de87c3a6d7866e4f310f1
unknown <surafelyimam.sy@gmail.com>
2023-09-02 15:17:50 +0300

    Fixing Errors

--- pages/products/solar-powered-water-pumping-systems/index.tsx
+++ pages/products/solar-powered-water-pumping-systems/index.tsx
@@ -18,31 +18,31 @@ export default function Index() {
                         autoplay={true}
                         loop={true}
                         autoplayDelay={3000}
-                        navigation={({ setActiveIndex, activeIndex, length }) => (
-                            <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-                                {new Array(length).fill("").map((_, i) => (
-                                    <span
-                                        key={i}
-                                        className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
-                                            }`}
-                                        onClick={() => setActiveIndex(i)}
-                                    />
-                                ))}
-                            </div>
-                        )}
+                        // navigation={({ setActiveIndex, activeIndex, length }) => (
+                        //     <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+                        //         {new Array(length).fill("").map((_, i) => (
+                        //             <span
+                        //                 key={i}
+                        //                 className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                        //                     }`}
+                        //                 onClick={() => setActiveIndex(i)}
+                        //             />
+                        //         ))}
+                        //     </div>
+                        // )}
                     >
                         <img
-                            src="/assets/products/solar-powered/panel.jpg"
+                            src="/assets/products/solar-powered/panel.JPG"
                             alt="image 1"
                             className="h-full w-full object-cover"
                         />
                         <img
-                            src="/assets/products/solar-powered/pump.jpg"
+                            src="/assets/products/solar-powered/pump.JPG"
                             alt="image 2"
                             className="h-full w-full object-cover"
                         />
                         <img
-                            src="/assets/products/solar-powered/spray.jpg"
+                            src="/assets/products/solar-powered/spray.JPG"
                             alt="image 3"
                             className="h-full w-full object-cover"
                         />
