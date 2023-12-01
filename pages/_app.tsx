a45cfbcbfb8e90f6c28b8ff61435a7a2a04cd635
admin <surafelyimam.sy@gmail.com>
2023-12-01 18:27:17 +0300

    Chaanged template

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
-
-// @material-tailwind/react
-import { ThemeProvider } from "@material-tailwind/react";
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
